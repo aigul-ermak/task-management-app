ac92413195f40f18d3180458cef569626f1fff93
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-07-03 15:30:29 -0300

    changed port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import {runDb} from "./config/db";
 import {app} from "./app";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const startApp = async (): Promise<void> => {
     await runDb()
