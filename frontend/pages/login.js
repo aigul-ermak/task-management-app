b5005cc1a03dee4b4d8363bbdace8fcf18699c51
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-07-03 16:26:08 -0300

    Updated login functionality

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import {useAuth} from "@/context/authContext";
 import {useRouter} from "next/router";
-import axios from "axios";
+import axios from "@/utils/axios";
 
 export default function Login() {
     const [email, setEmail] = useState('');
