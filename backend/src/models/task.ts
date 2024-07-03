7d31ecebc0a0ec20aec02ec665f1a1e08000aa74
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-07-03 16:45:04 -0300

    Created task model and type

---
+++ backend/src/models/task.ts
@@ -0,0 +1,34 @@
+import mongoose, {Document, Schema} from 'mongoose';
+import {TASK_STATUS, TaskType} from "../types/taskType";
+
+
+export interface ITask extends TaskType, Document {
+}
+
+const taskSchema: Schema = new mongoose.Schema<ITask>({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: Object.values(TASK_STATUS),
+        default: TASK_STATUS.PENDING,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export const Task = mongoose.model<ITask>('Task', taskSchema);
+
+
---
+++ backend/src/types/taskType.ts
@@ -0,0 +1,14 @@
+export enum TASK_STATUS {
+    PENDING = 'pending',
+    IN_PROGRESS = 'in-progress',
+    COMPLETED = 'completed'
+}
+
+
+export type TaskType= {
+    title: string;
+    description: string;
+    status: TASK_STATUS;
+    createdAt: Date;
+    updatedAt: Date;
+}
